0f2aa06e5afd31e6185b88e241a0f035de8d4f11
huangjiawei <>
2018-06-13 13:38:58 +0800

    增加web.xml初始化WebApplicationContext

---
+++ src/main/java/com/example/spring_mvc_test/bean/Student.java
@@ -0,0 +1,5 @@
+package com.example.spring_mvc_test.bean;
+
+public class Student {
+
+}
--- src/main/java/com/example/spring_mvc_test/controller/DemoController.java
+++ src/main/java/com/example/spring_mvc_test/controller/DemoController.java
@@ -1,14 +1,25 @@
 package com.example.spring_mvc_test.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.context.ContextLoader;
+import org.springframework.web.context.WebApplicationContext;
+
+import com.example.spring_mvc_test.bean.Student;
 
 @Controller
 @RequestMapping("/index")
 public class DemoController {
 
+	@Autowired
+	private Student student;
+
 	@RequestMapping("/index")
 	public String index() {
+		System.err.println(student);
+		WebApplicationContext webContext = ContextLoader.getCurrentWebApplicationContext();
+		System.err.println(webContext);
 		return "index";
 	}
 }
