1184186e8bc86ca2bfd0ebdd4ea3693c853b96bb
huangjiawei <>
2018-06-13 10:20:40 +0800

    初始提交

---
+++ src/main/java/com/example/spring_mvc_test/controller/DemoController.java
@@ -0,0 +1,14 @@
+package com.example.spring_mvc_test.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/index")
+public class DemoController {
+
+	@RequestMapping("/index")
+	public String index() {
+		return "index";
+	}
+}
